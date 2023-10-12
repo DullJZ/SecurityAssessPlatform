5742e3f04fe8e4910a6a535ecf8541812b6a00b1
DullJZ <79080562+DullJZ@users.noreply.github.com>
2023-10-12 23:36:57 +0800

    bug fix and requirements.txt added

--- docker-manager/main.py
+++ docker-manager/main.py
@@ -2,6 +2,9 @@ import docker
 import time
 import yaml
 import subprocess
+import sys
+import socket
+import random
 
 def GetDockerClient(remote = '') -> docker.DockerClient:
     if remote:
@@ -80,22 +83,25 @@ def ShowAllContainers(client) -> dict:
         return {'status': 'failed', 'message': e}
     return {'status': 'success', 'containers': containers}
 
-def RunContainerByComposeFile(client, compose_file_dir) -> dict:
+def RunContainerByComposeFile(client, compose_file_dir) -> list:
     """
     Only support docker-compose.yml version 3
     Only support 'image', 'container_name', 'restart', 'environment', 'volumes', 'ports' keys
     """
+    result = []
     with open(compose_file_dir, 'r') as f:
         container_info = yaml.load(f, Loader=yaml.FullLoader)
     container_info = container_info['services']
+    # think about more than one container situation
+    # use 'for' to run each single container
     for single_container in container_info.values():
         # image and pull
         if 'image' in single_container.keys():
             s = PullImage(client, single_container['image'])
             if s['status'] == 'failed':
-                return {'status': 'failed', 'message': s['message']}
+                result.append({'status': 'failed', 'message': s['message']})
         else:
-            return {'status': 'failed', 'message': 'No image assigned'}  # A MUST!
+            result.append({'status': 'failed', 'message': 'No image assigned'})  # A MUST!
         # container_name
         if 'container_name' in single_container.keys():
             container_name = single_container['container_name']
@@ -121,23 +127,27 @@ def RunContainerByComposeFile(client, compose_file_dir) -> dict:
         else:
             volumes = None
         # ports
+        host_ports = [] # use for log
         if 'ports' in single_container.keys():
             ports = dict()
             for mapping_rule in single_container['ports']:
                 host_port, container_port = mapping_rule.split(':')
+                host_port = GenerateUnusedPort()
+                host_ports.append(host_port)
                 ports[f'{container_port}/tcp'] = int(host_port)
         else:
             ports = None
-
-        # run container
+        # run a container
         try:
             client.containers.run(single_container['image'], detach=True, name=container_name,
                                   restart_policy=restart_policy, environment=environment, volumes=volumes, ports=ports)
         except Exception as e:
             print(e)
-            return {'status': 'failed', 'message': e}
+            result.append({'status': 'failed', 'message': e})
         print('Run container {} success'.format(container_name))
-        return {'status': 'success', 'ContainerName': container_name}
+        result.append({'status': 'success', 'ContainerName': container_name, 'Ports': host_ports})
+    # 'for' end
+    return result
 
 def RunCommand(command: str) -> dict:
     """
@@ -153,13 +163,22 @@ def RunCommand(command: str) -> dict:
     return {'status': 'success', 'result': result}
 
 def GenerateUnusedPort() -> int:
-    pass
+    while True:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        port = random.randint(10000, 65535)
+        location = ('127.0.0.1', port)
+        result = sock.connect_ex(location)
+        if result != 0:
+            # this port is not in use
+            break
+        sock.close()
+    return port
 
 
 # use for test
 client = GetDockerClient()
 print(GetDockerVersion(client))
 print(GetDockerInfo(client))
-#print(RunContainerByComposeFile(client, './test/docker-compose.yml'))
+print(RunContainerByComposeFile(client, './test/docker-compose.yml'))
 #ForceDeleteContainer(client, '1697105828')
-print(ShowAllContainers(client))
+#print(ShowAllContainers(client))
