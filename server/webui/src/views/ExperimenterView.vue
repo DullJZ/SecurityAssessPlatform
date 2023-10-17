bab2113f5340f4d41b99ef74b3b0979518a51c72
cyxxuan <434358903@qq.com>
2023-10-17 16:01:39 +0800

    前端工程初始化

---
+++ server/webui/src/views/ExperimenterView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Login As A Experimenter ...
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ server/webui/src/views/SupervisorView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <el-link>Login As A Supervisor ...</el-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
