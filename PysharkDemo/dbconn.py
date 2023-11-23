44c7178e70f43401ff8225d45131a8214ded1eb7
DullJZ <79080562+DullJZ@users.noreply.github.com>
2023-11-23 13:08:45 +0000

    PR conflict

--- PysharkDemo/dbconn.py
+++ PysharkDemo/dbconn.py
@@ -1,12 +1,11 @@
 import mysql.connector
 
 config = {
-    "host": "139.159.154.40",
-    "port": 30006,
-    "user": "pyshark_demo",
-    "password": "xhwZWNdD32rZnmPs",
-    "database": "pyshark_demo"
-}
+    "host": "localhost",
+    "port": 13306,
+    "user": "root",
+    "password": "root",
+    }
 conn = mysql.connector.connect(**config)
 
 print("数据库连接成功")
